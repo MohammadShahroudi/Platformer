c90673acdffaea592481667119efe8464a91c5fc
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-02-12 21:20:51 -0800

    Initial commit

---
+++ Assets/Platformer/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+
+    float timeLeft = 500;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeText.text = $"TIME\n {((int)timeLeft).ToString()}";
+    }
+}
